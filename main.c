bb205d1deac3080bea1bdad0352f57743632fbc9
Holly Finch <49234526+HollyFinch@users.noreply.github.com>
2024-09-02 15:21:45 +0100

    Add files via upload
    We start literally from nothing and create an interesting renderer. I avoid using OpenGL for the time being, instead my image will be rendered through a file.

--- main.c
+++ main.c
@@ -1,57 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "geometry.h"
-
-//forgot how to declare global var LOL
-#define width 500
-#define height 500
-
-int framebuffer[width][height]; 
-
-void rasterizeTriangle(Triangle triangle) {
-  // Calculate bounding box
-  int minX = floor(fmin(triangle.p1.x, fmin(triangle.p2.x, triangle.p3.x)));
-  int maxX = ceil(fmax(triangle.p1.x, fmax(triangle.p2.x, triangle.p3.x)));
-  int minY = floor(fmin(triangle.p1.y, fmin(triangle.p2.y, triangle.p3.y)));
-  int maxY = ceil(fmax(triangle.p1.y, fmax(triangle.p2.y, triangle.p3.y)));
-
-
-  for (int y = minY; y <= maxY; y++) {
-    for (int x = minX; x <= maxX; x++) {
-      if (isInsideTriangle(x, y, triangle)) {
-        framebuffer[y][x] = triangle.color;
-      }
-    }
-  }
-}
-
-bool isInsideTriangle(int x, int y, Triangle triangle) {
-  // Implement barycentric coordinate check here
-}
-
-void render(Triangle triangle) {
-  rasterizeTriangle(triangle);
-  // Display framebuffer (implementation depends on your chosen method)
-}
-
 
 int main() {
 
-  Point p1 = {100.0f, 50.0f, 0.0f};
-  Point p2 = {200.0f, 200.0f, 0.0f};
-  Point p3 = {50.0f, 200.0f, 0.0f};
-  Triangle triangle = {p1, p2, p3};
+    int image_W = 500;
+    int image_H = 500;
 
-  render(triangle);
-
-  
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      printf("%d ", framebuffer[y][x]); 
+    FILE *file = fopen("render_image.ppm", "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
     }
-    printf("\n");
-  }
 
-  return 0;
+    fprintf(file, "P3\n%d %d\n255\n", image_W, image_H);
+
+    for (int i = 0; i < image_W; i++) {
+        for (int j = 0; j < image_H; j++) {
+            fprintf(file, "255 0 0\n"); 
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
